e2cbfb325c3de561df518b05f5673b1c8f11c9ff
DiveXT <164129016+DiveXT@users.noreply.github.com>
2025-02-07 14:52:04 +0700

    first uncle update

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -59,7 +59,7 @@ exports.getCookiePolicy = async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: 'Error fetching bookings' });
     }
-  }
+  };
   
 exports.getLegalNotes = async (req, res) => {
     try {
@@ -67,8 +67,7 @@ exports.getLegalNotes = async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: 'Error fetching bookings' });
     }
-  }
-
+  };
   exports.getcustomerservice = async (req, res) => {
     try {
       res.render('pages/customerservice.ejs');
