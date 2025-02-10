6c05dbf4dd455495449d6e8b8ca7beaad54ed2c2
oakkharaphong <oakkharaphong1@gmail.com>
2025-02-10 10:41:15 +0700

    sucess profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -62,3 +62,77 @@ exports.changePhone = async (req, res) => {
   }
 };
 
+exports.changeCompany = async (req, res) => {
+  try {
+    const { currentInput } = req.body;
+
+    const user = await User.findOne(
+      { _id: req.user._id },
+      { _id: 1, companyName: 1 }
+    );
+    user.companyName = currentInput;
+
+    await user.save();
+
+    res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.log("Error Change Phone" ,error)
+    res.status(500).json({ message: "Error Change Phone" });
+  }
+};
+
+exports.changeBusiness = async (req, res) => {
+  try {
+    const { currentInput } = req.body;
+
+    const user = await User.findOne(
+      { _id: req.user._id },
+      { _id: 1, businessNumber: 1 }
+    );
+    user.businessNumber = currentInput;
+
+    await user.save();
+
+    res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.log("Error Change Phone" ,error)
+    res.status(500).json({ message: "Error Change Phone" });
+  }
+};
+exports.changeBusiness = async (req, res) => {
+  try {
+    const { currentInput } = req.body;
+
+    const user = await User.findOne(
+      { _id: req.user._id },
+      { _id: 1, businessNumber: 1 }
+    );
+    user.businessNumber = currentInput;
+
+    await user.save();
+
+    res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.log("Error Change Phone" ,error)
+    res.status(500).json({ message: "Error Change Phone" });
+  }
+};
+
+exports.changePerson = async (req, res) => {
+  try {
+    const { currentInput } = req.body;
+
+    const user = await User.findOne(
+      { _id: req.user._id },
+      { _id: 1, contactPerson: 1 }
+    );
+    user.contactPerson = currentInput;
+
+    await user.save();
+
+    res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.log("Error Change Phone" ,error)
+    res.status(500).json({ message: "Error Change Phone" });
+  }
+};
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -6,5 +6,8 @@ router.get('/', profileController.getProfile);
 router.post('/update', profileController.updateProfile);
 router.post('/change-password', profileController.changePassword);
 router.post('/change-phone', profileController.changePhone);
+router.post('/change-company', profileController.changeCompany);
+router.post('/change-business', profileController.changeBusiness);
+router.post('/change-person', profileController.changePerson);
 
 module.exports = router;
